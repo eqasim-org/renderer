d303ca6eaf551a83fb0bd36b33c94d256c7d22a8
Sebastian Hörl <hoerl.sebastian@gmail.com>
2020-12-01 19:47:18 +0100

    Odyssee

---
+++ src/main/java/ch/ethzm/matsim/renderer/RunToulouseVisualization.java
@@ -0,0 +1,65 @@
+package ch.ethzm.matsim.renderer;
+
+import java.util.Arrays;
+
+import ch.ethzm.matsim.renderer.config.ActivityConfig;
+import ch.ethzm.matsim.renderer.config.NetworkConfig;
+import ch.ethzm.matsim.renderer.config.RenderConfig;
+import ch.ethzm.matsim.renderer.config.VehicleConfig;
+import ch.ethzm.matsim.renderer.main.RunRenderer;
+
+public class RunToulouseVisualization {
+	static public void main(String[] args) {
+		/*-
+		 * This script writes a series of png files to an output directory,
+		 * visualizing a MATSim simulation. As input it needs a network file and an
+		 * events file from the MATSim output. Afterwards, using FFMPEG the png files
+		 * can be combined to a video with a command like this:
+		 * 
+		 * ffmpeg -framerate 25 -i video_%d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p output.mp4
+		 */
+
+		// START CONFIGURATION
+
+		RenderConfig renderConfig = new RenderConfig();
+
+		renderConfig.width = 1280;
+		renderConfig.height = 720;
+
+		renderConfig.networkPath = "/home/shoerl/Downloads/toulouse_output_5pct/output_network.xml.gz";
+		renderConfig.eventsPath = "/home/shoerl/Downloads/toulouse_output_5pct/output_events.xml.gz";
+		renderConfig.outputPath = "/home/shoerl/video/toulouse";
+
+		renderConfig.startTime = 8.0 * 3600.0;
+		renderConfig.endTime = 9.0 * 3600.0;
+		renderConfig.secondsPerFrame = 120.0;
+
+		renderConfig.showTime = false;
+
+		renderConfig.center = Arrays.asList(555438.0, 6245742.0);
+		renderConfig.zoom = 14000.0;
+
+		NetworkConfig roadNetwork = new NetworkConfig();
+		renderConfig.networks.add(roadNetwork);
+		roadNetwork.modes = Arrays.asList("car");
+		roadNetwork.color = Arrays.asList(200, 200, 200);
+
+		VehicleConfig generalVehicle = new VehicleConfig();
+		renderConfig.vehicles.add(generalVehicle);
+		generalVehicle.color = Arrays.asList(200, 200, 200);
+
+		VehicleConfig taxiVehicle = new VehicleConfig();
+		renderConfig.vehicles.add(taxiVehicle);
+		taxiVehicle.startsWith = Arrays.asList("taxi");
+		taxiVehicle.color = Arrays.asList(0, 0, 0);
+
+		ActivityConfig pudoActivity = new ActivityConfig();
+		renderConfig.activities.add(pudoActivity);
+		pudoActivity.types = Arrays.asList("pickup", "dropoff");
+		pudoActivity.color = Arrays.asList(7, 145, 222);
+
+		// END CONFIGURATION
+
+		RunRenderer.run(renderConfig);
+	}
+}
